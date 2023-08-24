685a4758ed1f0152aa8b3c395e351ada28feaa6b
will kim <78planet@gmail.com>
2023-08-24 14:46:43 +0900

    feat: add Redis Lock (Lettuce, Redisson Lock)

---
+++ src/main/java/com/will/stock/facade/RedissonLockStackFacade.java
@@ -0,0 +1,37 @@
+package com.will.stock.facade;
+
+import com.will.stock.service.StockService;
+import java.util.concurrent.TimeUnit;
+import lombok.RequiredArgsConstructor;
+import net.bytebuddy.asm.Advice.Return;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class RedissonLockStackFacade {
+
+    private final RedissonClient redissonClient;
+    private final StockService stockService;
+
+    public void decrease(Long key, Long quantity) {
+        RLock lock = redissonClient.getLock(key.toString());
+
+        try {
+            boolean available = lock.tryLock(10, 1, TimeUnit.SECONDS);
+
+            if (!available) {
+                System.out.println("lock 획득 실패");
+                return;
+            }
+
+            stockService.decrease(key, quantity);
+
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } finally {
+            lock.unlock();
+        }
+    }
+}
